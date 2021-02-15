a6954dcd90e061b4429a87ce2679755e5e378491
samshine <sameershinde23243@gmail.com>
2021-02-15 12:11:26 +0530

    use case 4,5 and 6 code added

--- example2.sh
+++ example2.sh
@@ -1,18 +1,17 @@
 echo "welcome to employee wage computation"
 
-part_time=1
-full_time=2
-absent=0
-part_time_hr=4
+daily_wage=160
 wage_per_hr=20
-full_time_hr=8
-randomNumber=$(($RANDOM%3))
-if [ $randomNumber -eq $part_time ]
-then
-	echo "part time wage:" $(($part_time_hr*$wage_per_hr))
-elif [ $randomNumber -eq $full_time ]
-then
-	echo "full time wage:" $(($full_time_hr*$wage_per_hr))
-else
-	echo "worker absent"
-fi
+echo "1:Wage per month(by day) 2:wage per month(by hr and day)"
+read -p "enter choice:" choice
+case $choice in
+   "1")
+      monthly_wage=$((20*$daily_wage))
+      echo "monthly  wage(by days):" $monthly_wage
+      ;;
+   "2")
+      monthly_wage=$((100*$wage_per_hr))
+      echo "monthly wage(by hours):" $monthly_wage
+      ;;
+esac
+
