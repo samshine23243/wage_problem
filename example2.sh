f2fa7e97eb42ebbc146949431277eb83af9a1496
samshine <sameershinde23243@gmail.com>
2021-02-15 11:48:09 +0530

    use case 1 code added

--- example2.sh
+++ example2.sh
@@ -1,1 +1,11 @@
 echo "welcome to employee wage computation"
+
+present=1
+absent=0
+randomNumber=$(($RANDOM%2))
+if [ $randomNumber -eq $present ]
+then
+	echo "worker present"
+else
+	echo "worker absent"
+fi
